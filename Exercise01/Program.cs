480205895d4e528b0e226002ede3933c192a2daa
hchoi84@gmail.comlszYeuzF3EErZcYc6exit <hchoi84@gmail.com>
2019-10-20 22:02:13 -0700

    Implemented users input to start, stop, or quit StopWatch

--- Exercise01/Program.cs
+++ Exercise01/Program.cs
@@ -8,26 +8,36 @@ namespace Exercise01
     static void Main(string[] args)
     {
       StopWatch sw = new StopWatch();
+      Random rand = new Random();
+      string startCmd = "start";
+      string stopCmd = "stop";
+      string quitCmd = "quit";
 
-      sw.Started = DateTime.Now;
-      Console.WriteLine("Time watch started at: {0}", sw.Started.ToString("HH:mm:ss.fffffff"));
+      while (true)
+      {
+        Console.WriteLine($"Instruction: \"{startCmd}\", \"{stopCmd}\", \"{quitCmd}\"");
+        string input = Console.ReadLine();
 
-      sw.Stopped = DateTime.Now;
-      Console.WriteLine("Time watch stopped at: {0}", sw.Stopped.ToString("HH:mm:ss.fffffff"));
+        if (input == startCmd)
+        {
+          sw.Started = DateTime.Now;
+          Console.WriteLine("Time watch started at: {0}", sw.Started.ToString("HH:mm:ss.fffffff"));
+        }
 
-      Console.WriteLine("Duration: {0}", sw.Duration.ToString());
+        if (input == stopCmd)
+        {
+          sw.Stopped = DateTime.Now;
+          Console.WriteLine("Time watch stopped at: {0}", sw.Stopped.ToString("HH:mm:ss.fffffff"));
 
-      Console.WriteLine();
+          Console.WriteLine("Duration: {0}", sw.Duration.ToString());
+        }
 
-      Thread.Sleep(3000);
-      sw.Started = DateTime.Now;
-      Console.WriteLine("Time watch started at: {0}", sw.Started.ToString("HH:mm:ss.fffffff"));
+        if (input == quitCmd)
+        {
+          break;
+        }
+      }
 
-      Thread.Sleep(3000);
-      sw.Stopped = DateTime.Now;
-      Console.WriteLine("Time watch stopped at: {0}", sw.Stopped.ToString("HH:mm:ss:fffffff"));
-
-      Console.WriteLine(@"Duration: {0:ss\.fffffff}", sw.Duration);
       // For TimeSpan, the string format specifier is slightly different. It utilizes literals and escape characters
     }
   }
